239941b3e56a155f0df6c8bfe36ecd89dc173516
Avinash Krishna <45009847+avinashverse@users.noreply.github.com>
2025-01-18 18:01:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Smart-stock
+# StockPricePredictor
+A deep learning project to predict stock price of NSI companies.
+App Link: https://stockpricepredictor-gmtpairrk2h4ns2awgmdwi.streamlit.app/
---
+++ 📈_Homepage.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+import requests
+
+st.set_page_config(
+    page_title="💸Homepage",
+    initial_sidebar_state="expanded",
+    layout="wide"
+)
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.title('Welcome to Stock Price Predictor!')
+    st.write("Presenting Stock Price Predictor - The Golden Path to Stock Prognostication.We are here to assist you every step of the way, providing invaluable insights and empowering you to make informed decisions. Additionally, our expertise extends to predicting closing prices, equipping you with a strategic advantage in optimizing your investment strategies. Let us be your trusted companion on this thrilling journey, as we empower you to unlock the full potential of your financial aspirations.")
+with col2:
+    stocks=load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_kuhijlvx.json")
+    st_lottie(stocks)
